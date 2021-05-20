8d2d81af186a1856245b28b90362de9e63ffea77
Veronica Gatica <80833103+Verogatica@users.noreply.github.com>
2021-05-20 00:09:10 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function primeraRevista() {
-    document.getElementsByTagName("body")[0].style.background = "#D34145";
+    document.getElementsByTagName("body")[0].style.background = "#FFD7F4";
 }
 function segundaRevista() {
-    document.getElementsByTagName("body")[0].style.background = "#5790A3";
+    document.getElementsByTagName("body")[0].style.background = "#faf5d7";
 }
 function terceraRevista() {
-    document.getElementsByTagName("body")[0].style.background = "#6A5D9C";
+    document.getElementsByTagName("body")[0].style.background = "#c8954b";
 }
 function cuartaRevista() {
-    document.getElementsByTagName("body")[0].style.background = "#117C60";
+    document.getElementsByTagName("body")[0].style.background = "#939c95";
 }
 function nada() {
     document.getElementsByTagName("body")[0].style.background = "#222";
